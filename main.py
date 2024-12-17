8e1e4b945a46e165e1e33fd8680f7399382348f3
ZetsubouBilly <KuroNeko13666@mail.ru>
2024-12-17 16:23:24 +0300

    добавил jinja2 и несколько шаблонов для визуализации записей

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from typing import List
-from fastapi import Body, FastAPI, HTTPException, status
+from fastapi import Body, FastAPI, HTTPException, status, Request
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 class Message(BaseModel):
@@ -11,10 +14,10 @@ class Message(BaseModel):
     text: str
 
     model_config = {
-        'json_schema_extra': {
-            'examples': [
+        "json_schema_extra": {
+            "examples": [
                 {
-                    'text': 'Simple message',
+                    "text": "Simple message",
                 }
             ]
         }
@@ -25,14 +28,18 @@ message_db = []
 
 
 @app.get("/")
-async def get_all_messages() -> List[Message]:
-    return message_db
+async def get_all_messages(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(
+        "message.html", {"request": request, "messages": message_db}
+    )
 
 
 @app.get("/message/{message_id}")
-async def get_message(message_id: int) -> Message:
+async def get_message(request: Request, message_id: int) -> Message:
     try:
-        return message_db[message_id]
+        return templates.TemplateResponse(
+            "message.html", {"request": request, "message": message_db[message_id]}
+        )
     except IndexError:
         raise HTTPException(status_code=404, detail="Message not found")
 
@@ -42,9 +49,9 @@ async def create_message(message: Message) -> str:
     if len(message_db) == 0:
         message.id = 0
     else:
-        message.id = max([i.dict()['id'] for i in message_db]) + 1
+        message.id = max([i.dict()["id"] for i in message_db]) + 1
     message_db.append(message)
-    return 'Message created'
+    return "Message created"
 
 
 @app.put("/message/{message_id}")
@@ -52,7 +59,7 @@ async def update_message(message_id: int, message: str = Body()) -> str:
     try:
         edit_message = message_db[message_id]
         edit_message.text = message
-        return f'Message {message_id} updated'
+        return f"Message {message_id} updated"
     except IndexError:
         raise HTTPException(status_code=404, detail="Message not found")
 
@@ -61,7 +68,7 @@ async def update_message(message_id: int, message: str = Body()) -> str:
 async def delete_message(message_id: int) -> str:
     try:
         message_db.pop(message_id)
-        return f'Message {message_id} deleted'
+        return f"Message {message_id} deleted"
     except IndexError:
         raise HTTPException(status_code=404, detail="Message not found")
 
